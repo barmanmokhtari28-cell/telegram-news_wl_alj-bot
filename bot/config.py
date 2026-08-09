53b083323a01ee8ec93e7e7aee3ec99a8852bac8
barmanmokhtari28-cell <barmanmokhtari28@gmail.com>
2026-08-09 16:49:42 +0330

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,21 +3,23 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID", "@secretollah")
 
-# Keywords to match (Case-insensitive)
 TARGET_KEYWORDS = [
     "iran", "tehran",
     "us", "u.s.", "united states", "washington", "biden",
     "trump", "donald trump"
 ]
 
-# Direct feeds from outlets
 FEEDS = {
     "AlJazeera": {
         "url": "https://www.aljazeera.com/xml/rss/all.xml",
         "hashtag": "#الجزیره"
     },
-    "WSJ": {
+    "WSJ_World": {
         "url": "https://feeds.a1.wsj.net/xml/rss/3_7085.xml",
+        "hashtag": "#وال_استریت_جورنال"
+    },
+    "WSJ_US": {
+        "url": "https://feeds.a1.wsj.net/xml/rss/3_7014.xml",
         "hashtag": "#وال_استریت_جورنال"
     }
 }
